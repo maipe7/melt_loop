01770894842eadceb17abbc617d057bf71931674
maipe7 <maipe@seznam.cz>
2024-04-05 17:28:05 +0200

    script cleaning

--- plots/addblank_gnuplot.sh
+++ plots/addblank_gnuplot.sh
@@ -1,12 +1,12 @@
-folder=../outputs/thermal-melt-petrol-chwide-bm-k0-r2
-#folder=thermal-melt-petrol-chwide-th-et11-c06-cool-k7
- awk -f addblanks.awk < $folder/depth_average.txt > ../outputs/d.txt
- rm -r ../outputs/particles/; mkdir ../outputs/particles
- #cp $folder/particles/*.0000.gnuplot ./particles/
-pwd
- cp $folder/particles/*0.0000.gnuplot ../outputs/particles/
+folder0=../outputs
+folder1=thermal-melt-bm-k8-v0
+folder=$folder0/$folder1
+ awk -f addblanks.awk < $folder/depth_average.txt > $folder0/d.txt
+ rm -r $folder0/particles/; mkdir $folder0/particles
+ cp $folder/particles/*.0000.gnuplot $folder0/particles/
+ #cp $folder/particles/*0.0000.gnuplot $folder0/particles/
 
-for particlefile in ../outputs/particles/*; do
+for particlefile in $folder0/particles/*; do
  sort -t' ' -b -nk3 $particlefile --output=${particlefile}s
 done
  gnuplot -e "titlem='${folder: 31}'" -p 'T-phi-c.plt'
